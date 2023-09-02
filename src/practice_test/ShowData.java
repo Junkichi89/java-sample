b1994e0112870bd788248aca50790790093b7696
Jumpei Asanuma <jun64pei89@gmail.com>
2023-09-02 16:24:00 +0900

    ShowDataの問題・回答

---
+++ src/practice_test/ShowData.java
@@ -0,0 +1,19 @@
+package practice_test;
+
+public class ShowData {
+	public static void main(String[] args) {
+		//変数の宣言と値の代入
+		int num = 10;
+		double pi = 3.14;
+		char blood = 'A';
+		boolean fun = true;
+		String msg = "ポケモンゲットだぜ！";
+		
+		//値の表示
+		System.out.println(num);
+		System.out.println(pi);
+		System.out.println(blood);
+		System.out.println(fun);
+		System.out.println("一緒に頑張りましょう！");
+	}
+}

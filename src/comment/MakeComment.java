8e6a1388eae3437255cf9fe8271ad8a9a911be22
Jumpei Asanuma <jun64pei89@gmail.com>
2023-09-02 13:27:24 +0900

    Javaの命名ルール
    1文字目
    クラス・インターフェース：　大文字
    変数・メソッド・パッケージ：　小文字
    2つ目以降の単語は大文字から

---
+++ src/comment/MakeComment.java
@@ -0,0 +1,15 @@
+package comment;
+
+public class MakeComment {
+//	コメントを記載してみました。
+/*
+ *複数行のコメントも入れました！
+ * 
+ */
+
+	public static void main(String[] args) {
+		System.out.println("コメントなので実行処理に影響を与えません"); // コメントなので無視されます！
+	}
+
+	
+}

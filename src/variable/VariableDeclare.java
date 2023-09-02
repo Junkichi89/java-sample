22269373699bf407b0609fa07e491d999c35b361
Jumpei Asanuma <jun64pei89@gmail.com>
2023-09-02 14:02:53 +0900

    変数
    変数は、データの入れ物
    
    変数名のルール
    - 1文字目
    　英字(a~z, A~Z),_,$
    - 2文字目以降は数字も可
    - キーワードは使えない
    
    変数の使い方
    
    1. 宣言する: 変数を作る
     データ型, 変数名, 変数名、・・・・・；
     (int month, day;)
    
     2. 代入する：値を入れる
     変数名 = 値; (age = 20;)
     3. 参照する: 値をみる
    
     1 + 2 初期化する
     int age = 20;

---
+++ src/variable/VariableDeclare.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class VariableDeclare {
+	public static void main (String[] args) {
+		int month;
+		month = 9;
+		int day = 16;
+		
+		System.out.println("このチャンネルは、");
+		System.out.println(month + "月" + day + "日に公開しました。");
+	}
+
+}

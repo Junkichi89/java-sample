e76cfc8f424779f5792c4ac5820637c753b68c2c
Jumpei Asanuma <jun64pei89@gmail.com>
2023-09-02 16:13:38 +0900

    コマンドラインからのデータ入力
    javaコマンド実行時にプログラムにデータを渡せる
    
    java CmdLine Hello Java
    
    Eclipseで引数を渡して実行したい場合は、実行の構成を確認して、
    引数を指定することが可能
    https://www.kenschool.jp/blog/?p=6611

---
+++ src/array/CommandData.java
@@ -0,0 +1,9 @@
+package array;
+
+public class CommandData {
+  public static void main(String[] args) {
+  	System.out.println("args[0]：" + args[0]);
+  	System.out.println("args[1]：" + args[1]);
+  	System.out.println("args.length：" + args.length);
+  }
+}

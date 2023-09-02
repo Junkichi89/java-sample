1f79d576751752b0ed7704a28000e9186a95b365
Jumpei Asanuma <jun64pei89@gmail.com>
2023-09-02 16:19:45 +0900

    GreetingBugのエラー解消
    pubric→　public

---
+++ src/practice_test/GreetingBug.java
@@ -0,0 +1,18 @@
+package practice_test;
+/* 
+pubricがタイポしている
+public
+public class GreetingBug {
+	pubric static void main(String[] args) {
+		System.out.println("こんにちは。");
+		System.out.println("一緒に頑張りましょう！");
+	}
+}
+*/
+
+public class GreetingBug {
+	public static void main(String[] args) {
+		System.out.println("こんにちは。");
+		System.out.println("一緒に頑張りましょう！");
+	}
+}
